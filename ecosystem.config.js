a096b41fdb0034a6cc1c814e05a4feaada18c3eb
lakheraakshay <akshay.lakhera@easystepin.com>
2022-12-30 18:17:11 +0530

    server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,12 @@
  *  production mode :: pm2 start ecosystem.config.js --only prod
  *  development mode :: pm2 start ecosystem.config.js --only dev
  */
- module.exports = {
+module.exports = {
   apps: [
     {
       name: 'prod', // pm2 start App name
-      script: 'dist/server.js',
+      // script: 'dist/server.js',
+      script: 'server.js',
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
       instances: 2, // pm2 instance count
@@ -19,7 +20,8 @@
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
+      env: {
+        // environment variable
         PORT: 3000,
         NODE_ENV: 'production',
       },
@@ -38,7 +40,8 @@
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
-      env: { // environment variable
+      env: {
+        // environment variable
         PORT: 3000,
         NODE_ENV: 'development',
       },
@@ -50,7 +53,8 @@
       host: '0.0.0.0',
       ref: 'origin/master',
       repo: 'git@github.com:repo.git',
-      path: 'dist/server.js',
+      // path: 'dist/server.js',
+      path: 'server.js',
       'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
     },
   },
